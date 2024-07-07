3a99481a131d5254bbeab2460f2be9a8f90b95d1
Ismail Faizan <61406671+Faizan-5zy@users.noreply.github.com>
2024-07-07 13:41:48 +0530

    Add files via upload

---
+++ 7-splice_emptyingArray.js
@@ -0,0 +1,24 @@
+array = ['hai','hello','bye']
+
+console.log(array)
+
+// array=[]
+
+// array.length=0
+
+
+// array.splice(0,array.length)
+
+// splice example 
+// const food = ['pizza', 'cake', 'salad', 'cookie'];
+// food.splice(2,1)
+// console.log(food)
+
+while (array.length=0){
+    array.pop()
+}
+
+
+
+
+console.log(array)
