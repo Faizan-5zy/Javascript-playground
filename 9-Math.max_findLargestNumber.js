0f41dfaf208e59fe77c94259b7a7d713f58ecab6
Ismail Faizan <61406671+Faizan-5zy@users.noreply.github.com>
2024-07-10 12:20:05 +0530

    Add files via upload

---
+++ 9-Math.max_findLargestNumber.js
@@ -0,0 +1,8 @@
+num1 = prompt("Enter num1:");
+num2 = prompt("Enter num2:");
+num3 = prompt("Enter num3:");
+
+largestNumber = Math.max(num1, num2, num3);
+console.log("🚀 ~ largestNumber:", largestNumber);
+
+// The Math.max function in JavaScript is used to find the largest number among the arguments provided. This function is part of the Math object, which contains various mathematical constants and functions.
